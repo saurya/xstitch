88dd58d6b645c3cbc7dc4e7e7995e96dc875031e
Saurya Velagapudi <saurya@gmail.com>
2013-01-01 19:43:09 -0500

    Fix bug where urls on / would fail to load due to non-existent path.

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        key = db.Key.from_path('Pattern', self.request.path[1:])
-        pattern = models.Pattern.all().filter('__key__ =', key).get()
+        path = self.request.path
         loadedStack = '[]'
         loadState = 'false'
-        if pattern:
-            loadState = 'true'
-            loadedStack = pattern.stack
+        if len(path) > 1:
+            key = db.Key.from_path('Pattern', path[1:])
+            pattern = models.Pattern.all().filter('__key__ =', key).get()
+            if pattern:
+                loadState = 'true'
+                loadedStack = pattern.stack
         template_values = { 'loadState' : loadState, 'stack' : loadedStack }
         path = os.path.join(os.path.dirname(__file__), 'cross.html')
         self.response.out.write(template.render(path, template_values))
 
+# TODO (saurya): Make a separate handler for / and /.*
 app = webapp2.WSGIApplication([
     ('/.*', MainHandler)
 ], debug=True)
